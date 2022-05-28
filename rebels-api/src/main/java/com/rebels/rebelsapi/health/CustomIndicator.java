83f7345b27f2b0ec0d8459394b3aa2f8f7769e03
= <=>
2022-05-28 00:08:23 -0300

    feat: adding prometheus

--- rebels-api/src/main/java/com/rebels/rebelsapi/health/CustomIndicator.java
+++ rebels-api/src/main/java/com/rebels/rebelsapi/health/CustomIndicator.java
@@ -4,20 +4,12 @@ import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 @Component("minhaPropriaMetrica")
 public class CustomIndicator implements HealthIndicator {
 
     @Override
     public Health health() {
-        boolean condicao = condicao();
-        if(condicao){
-            return Health.up().withDetail("Minuto par", 113).build();
-        } else return Health.down().withDetail("Minuto ímpar", 114).build();
-    }
-
-    private boolean condicao() {
-        return LocalDateTime.now().getMinute() % 2 == 0;
+            return Health.up().withDetail("true", 113).build();
     }
 
 }
